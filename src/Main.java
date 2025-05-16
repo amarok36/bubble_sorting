58b99083741045407d0d7b466f873211329bbb2e
amarok36 <bobrowww@mail.ru>
2025-05-16 19:02:16 +0500

    Moving the try-catch construct to the sortArray method

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,12 @@ public class Main {
             System.out.println("Инициализирован массив: ");
             display(nums);
 
-        try {
             int[] sortedNums = sortArray(nums);
             System.out.println("\nПроизведена сортировка.");
 
             System.out.println("\nОтсортированный массив: ");
             display(sortedNums);
-
-        } catch (Exception e) {
-            System.err.println("Ошибка: " + e.getMessage());
         }
-    }
 
     private static void display(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -34,15 +29,22 @@ public class Main {
     }
 
     public static int[] sortArray(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j + 1] < arr[j]) {
-                    int swap = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = swap;
+        try{
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j < arr.length - i - 1; j++) {
+                    if (arr[j + 1] < arr[j]) {
+                        int swap = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = swap;
+                    }
                 }
             }
+        } catch(ArrayIndexOutOfBoundsException ex) {
+            System.err.println("Ошибка.Выход за пределы массива");
+        } catch (Exception e) {
+                System.err.println("Ошибка. " + e.getMessage());
         }
         return arr;
     }
 }
+
